0ea0ae2281d6b015ec4ac5bf33a3f2daa8594e61
richardjtelford <richard.telford@bio.uib.no>
2020-10-07 12:43:18 +0200

    fixing ggvegan dependency

--- R/zzz.R
+++ R/zzz.R
@@ -6,6 +6,7 @@
   if(rstudioapi::versionInfo()$version < 1.3){
     warning("You have an old version of Rstudio which may not completely support learnr tutorial.\nPlease update if possible")
   } else {
-    packageStartupMessage("Available tutorials shold show up in the Tutorial tab.\nNormally this is in the top right pane next to the Environment and History tabs")
+    packageStartupMessage("Available tutorials shold show up in the Tutorial tab.\nNormally this is in the top right pane next to the Environment and History tabs\nAlternatively, find available packages with\nlearnr::available_tutorials(package = \"Bio250\") \nand launch them with \nlearnr::run_tutorial(..., package = \"Bio250\")\nwhere ... is the name of the tutorial")
+    
   }
 }
